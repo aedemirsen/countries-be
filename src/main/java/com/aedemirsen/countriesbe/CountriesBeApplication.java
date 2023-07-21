a490a9bcdaf0e7e243e3a3d6d4af9c0bfd4c6027
aedemirsen <aedemirsen@gmail.com>
2023-07-21 19:31:34 +0300

    proje yapısı oluşturuldu

---
+++ src/main/java/com/aedemirsen/countriesbe/CountriesBeApplication.java
@@ -0,0 +1,13 @@
+package com.aedemirsen.countriesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aedemirsen/countriesbe/CountriesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aedemirsen.countriesbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
